ad97b96ba9c536c5729d25c0fb712c07034f273a
nikhil.ponugoti <nikhil.ponugoti@gmail.com>
2016-11-10 12:43:17 -0600

    display list of movies with buttons to change the rating

--- movies-list-component.js
+++ movies-list-component.js
@@ -1,17 +1,37 @@
-(function(){
+(function() {
   var module = angular.module("favMoviesList");
-  
-  module.component("moviesList",{
-    templateUrl:"movies-list-component.html",
-    controllerAs:"model",
-    controller: function(){
-      var model = this;
-      model.message="helooo from controller";
-      
-      model.change = function(){
-        model.msg="Howdy mate";
-      };
+
+  function fetchMovies($http) {
+    return $http.get("movies.json")
+      .then(function(response) {
+        return response.data;
+      });
+  }
+
+  function controller($http) {
+    var model = this;
+
+    model.$onInit = function() {
+      fetchMovies($http).then(function(movies) {
+        model.movies = movies;
+      });
+                         
+    };
+    
+    model.upRating = function(x){
+      if(x.rating<5)
+        x.rating +=1;
     }
+    model.downRating = function(x){
+      if(x.rating>1)
+        x.rating -=1;
+    }
+  }
+
+  module.component("moviesList", {
+    templateUrl: "movies-list-component.html",
+    controllerAs: "model",
+    controller: ["$http", controller]
+
   });
-   
 }());
