f7c80e3e590cba3c3f46376bd7e2e65b19a97efc
nikhil.ponugoti <nikhil.ponugoti@gmail.com>
2016-11-11 03:48:44 -0600

    Added routing using original router

--- module.js
+++ module.js
@@ -1,5 +1,19 @@
 
 (function(){
-  angular.module("favMoviesList",[]);
+  var module =angular.module("favMoviesList",["ngRoute"]);
   
+  module.config(function($routeProvider){
+    $routeProvider
+      .when("/list",{
+        template:"<movies-list></movies-list>"
+      })
+      .when("/about",{
+        template:"<app-about></app-about>"
+      })
+      .otherwise({redirectTo:"/list"})
+  });
+  
+  module.component("appAbout",{
+    template:"This is about page"
+  });
 }());
